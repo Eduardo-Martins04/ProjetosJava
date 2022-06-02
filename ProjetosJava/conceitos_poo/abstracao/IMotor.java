9fd7deefa6db333168b43e73dac0c51e8508f1e8
Eduardo Martins <80276944+Eduardo-Martins04@users.noreply.github.com>
2022-06-02 09:41:38 -0300

    Delete ProjetosJava/conceitos_poo directory

--- ProjetosJava/conceitos_poo/abstracao/IMotor.java
+++
@@ -1,8 +0,0 @@
-package formacao.desenvolvedores.tecnologia.uno.projetodesistemasorientadoaobjetos.conceitos_poo.abstracao;
-
-public interface IMotor {
-    public String getModelo(); // nao tem a implementacao do metodo {}
-
-    public String getFabricante();
-
-}
